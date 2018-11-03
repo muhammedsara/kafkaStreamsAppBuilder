c4a9fb324f0b7dd75580599bd0ad9f3f02029fcf
MerveYenice <onureozcan@gmail.com>
2018-11-03 13:50:18 +0300

    initial

---
+++ src/main/java/org/zero/kafkastreamsappbuilder/KafkastreamsappbuilderApplication.java
@@ -0,0 +1,12 @@
+package org.zero.kafkastreamsappbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkastreamsappbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkastreamsappbuilderApplication.class, args);
+	}
+}

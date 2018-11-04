a47ea6542915d72bc89f344e246a3ba274ba1f2f
onur <onur@dilisim.com>
2018-11-04 13:05:55 +0300

    templating

--- src/main/java/org/zero/kafkastreamsappbuilder/controllers/AppsController.java
+++ src/main/java/org/zero/kafkastreamsappbuilder/controllers/AppsController.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Map;
 
 @Controller
-public class IndexController {
+public class AppsController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Map<String, Object> model){
+        return "fragments/index";
+    }
 
-        return "blank";
+    @RequestMapping(value = "/app/create", method = RequestMethod.GET)
+    public String newApp(Map<String, Object> model){
+        return "fragments/create_app";
     }
 
 

c99f61c251957d956dbeb611a9cd436d1e0addbe
MerveYenice <merveyenice@anadolu.edu.tr>
2018-11-06 20:31:23 +0300

    bind ui from ajax response

--- src/main/resources/static/js/createApp.js
+++ src/main/resources/static/js/createApp.js
@@ -7,18 +7,28 @@ var cy = cytoscape({
 cy.on('click', 'node', function (evt) {
     console.log(evt.target);
 });
-var btnAddSrc = document.getElementById('addSource');
-btnAddSrc.addEventListener('click', function () {
-    cy.add({
-        group: "nodes",
-        data: {weight: 75},
-        position: {x: 200, y: 200}
-    });
-});
 
 var updateOpertaionsPanel = function(sourceOpId){
     $.getJSON("/rest/getavailableoperations?sId="+(sourceOpId || ""), function(data){
         console.log(data);
+        for(var item in data){
+            var name = data[item].name;
+            var btn = document.createElement("BUTTON");
+            btn.innerText = name;
+            btn.classList += "btn btn-info";
+            document.getElementById("tools_inner").appendChild(btn);
+            btn.addEventListener('click', function () {
+                cy.add({
+                    group: "nodes",
+                    data: {label: name},
+                    position: {x: 200, y: 200}
+                });
+            });
+            cy.style().selector('node')
+                .style({
+                    'label': 'data(label)'
+                }).update()
+        }
     });
 }
 updateOpertaionsPanel();

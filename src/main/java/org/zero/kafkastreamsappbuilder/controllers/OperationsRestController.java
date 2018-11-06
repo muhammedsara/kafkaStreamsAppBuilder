a8efb8c851a7d544119faa30554f2fdd52ceb234
MerveYenice <merveyenice@anadolu.edu.tr>
2018-11-06 19:29:53 +0300

    operations restController

---
+++ src/main/java/org/zero/kafkastreamsappbuilder/controllers/OperationsRestController.java
@@ -0,0 +1,30 @@
+package org.zero.kafkastreamsappbuilder.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.zero.kafkastreamsappbuilder.jpa.OperatorRepository;
+import org.zero.kafkastreamsappbuilder.models.OperatorModel;
+
+import java.util.List;
+
+@RestController
+public class OperationsRestController {
+
+    @Autowired
+    private OperatorRepository operatorRepository;
+
+    /**
+     * Get Available Operations on a Given Type.
+     * @param sId id of source type.
+     * @return list of available operations.
+     */
+    @RequestMapping("rest/getavailableoperations")
+    public List<OperatorModel> getAvailableOperations(@RequestParam("sId") String sId){
+
+        return operatorRepository.findOperatorModelBySourceType_Id(Integer.parseInt(sId));
+    }
+
+}
--- src/main/java/org/zero/kafkastreamsappbuilder/jpa/OperatorRepository.java
+++ src/main/java/org/zero/kafkastreamsappbuilder/jpa/OperatorRepository.java
@@ -3,5 +3,9 @@ package org.zero.kafkastreamsappbuilder.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zero.kafkastreamsappbuilder.models.OperatorModel;
 
+import java.util.List;
+
 public interface OperatorRepository extends JpaRepository<OperatorModel,Integer> {
+
+    List<OperatorModel> findOperatorModelBySourceType_Id(int id);
 }

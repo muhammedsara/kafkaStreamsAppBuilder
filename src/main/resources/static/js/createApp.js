69a542d336b801c6d05bc53048f90ec9f77f60e3
MerveYenice <merveyenice@anadolu.edu.tr>
2018-11-07 22:32:23 +0300

    made edges

--- src/main/resources/static/js/createApp.js
+++ src/main/resources/static/js/createApp.js
@@ -1,14 +1,29 @@
+var operationId;
+var nodeCount = 0;
+var lastClikedCy;
+
 var windowH = window.innerHeight;
 document.body.innerHTML += "<style>#cy{height: " + windowH * .8 + "px}</style>"
 var cy = cytoscape({
     container: document.getElementById('cy'),
     elements: []
 });
+// any click on canvas makes toolbox as default
+cy.on('click', function (event) {
+    var evtTarget = event.target;
+    if (evtTarget === cy) {
+        lastClikedCy = null;
+        updateOpertaionsPanel();
+    }
+});
+
 cy.on('click', 'node', function (evt) {
+    lastClikedCy = evt.target;
     $('#propBody').remove();
-    var operationId = this.data().operatorId;
+    operationId = this.data().operatorId;
+    var returnTypeId = this.data().returnTypeId;
     $.getJSON("/rest/getavailableproperties?opId=" + (operationId || ""), function (data) {
-        console.log(data);
+        //console.log(data);
         var ht = "<tbody id='propBody'></tbody>";
         $('#propTable').append(ht);
         for (var item in data) {
@@ -17,29 +32,48 @@ cy.on('click', 'node', function (evt) {
             $('#propTable').append(html);
         }
 
-    })
+    });
+
+    updateOpertaionsPanel(returnTypeId);
 
 
 });
 
 var updateOpertaionsPanel = function (sourceOpId) {
     $.getJSON("/rest/getavailableoperations?sId=" + (sourceOpId || ""), function (data) {
-        console.log(data);
+        //console.log(data);
+
         for (var item in data) {
             var name = data[item].name;
             var btn = document.createElement("BUTTON");
             btn.innerText = name;
             btn.classList += "btn btn-info";
+            document.getElementById("tools_inner").innerHTML = "";
             document.getElementById("tools_inner").appendChild(btn);
             btn.addEventListener('click', function () {
-                cy.add({
+                console.log("source: " + operationId + " target " + data[item].id);
+                var cyData = [{
                     group: "nodes",
                     data: {
+                        id: nodeCount,
                         label: name,
-                        operatorId: data[item].id
+                        operatorId: data[item].id,
+                        returnTypeId: data[item].returnType.id
+
                     },
                     position: {x: 200, y: 200}
-                });
+                }];
+                if (lastClikedCy) {
+                    cyData.push({
+                        group: "edges",
+                        data: {
+                            source: lastClikedCy.id(),
+                            target: nodeCount
+                        }
+                    });
+                }
+                cy.add(cyData);
+                nodeCount++;
             });
             cy.style().selector('node')
                 .style({
